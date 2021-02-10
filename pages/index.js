c3d8ce70e6f2ebe7ae77fd972af683ba5111d084
Paulo Alenquer <pauloalenquer@gmail.com>
2021-02-10 01:06:27 -0300

    added content in homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      <h1>Home</h1>
+    <div className="flex flex-col justify-center items-center w-full h-full text-gray-700 leading-loose">
+      <div className="max-w-xs">
+        <h2 className="text-purple-500 text-2xl font-bold">Hello I'm</h2>
+        <h1 className="text-6xl font-Teko font-black text-gray-800">
+          PAULO ALENQUER
+        </h1>
+        <h3 className="font-semibold">Web Developer</h3>
+        <h4 className="text-gray-500 text-sm">
+          My name is Paulo Alenquer, I'm 23 years old, I love Technology,
+          Programming and Art.
+        </h4>
+        <Link href="/about">
+          <a className="block bg-purple-500 w-28 mt-8 rounded-md h-12 text-white transform hover:rotate-6 hover:shadow-3xl transition duration-500 focus:outline-none text-center leading-12">
+            Hire me
+          </a>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    boxShadow: {
+     '3xl': '8px 8px 6px -6px rgba(0, 0, 0, 0.75)',
+    },
     extend: {
+      lineHeight: {
+        '12': '3rem',
+       },
       fontFamily: {
         Montserrat: ["Montserrat, sans-serif"],
-        Teko: ['Teko', 'sans-serif'],
+        Teko: ["Teko", "sans-serif"],
       },
     },
   },
   variants: {
     extend: {
-      backgroundColor: ['active'],
-    }
+      transitionProperty: ["hover", "focus"],
+      transform: ["hover", "focus"],
+    },
   },
   plugins: [],
 };
