e4760e20ec1d711f988290841a3051ea04a9ce35
Paulo Alenquer <pauloalenquer@gmail.com>
2021-02-20 19:31:04 -0300

    added content in contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,29 @@
+import SvgEmail from "../components/Contact/SvgEmail";
+import SvgLinkedin from "../components/Contact/SvgLinkedin";
+import SvgPhone from "../components/Contact/SvgPhone";
+import SvgGithub from "../components/Contact/SvgGithub";
+
 export default function Contact() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      <h1>Contact</h1>
-    </div>
+    <main className="flex flex-col justify-center items-center w-full min-h-screen text-gray-700 bg-purple-400 leading-loose px-4">
+      <ul className="flex flex-col justify-center items-center text-white w-full md:w-1/2 h-96">
+        <li className="flex items-center">
+          <SvgEmail />
+          <span className="ml-2 mb-2">pauloalenquer@gmail.com</span>
+        </li>
+        <li className="flex items-center">
+          <SvgPhone />
+          <span className="ml-2">(21) 96932-8671</span>
+        </li>
+        <ul className="flex justify-center items-center w-full mt-4">
+          <li className="mr-4">
+            <SvgLinkedin />
+          </li>
+          <li className="">
+            <SvgGithub />
+          </li>
+        </ul>
+      </ul>
+    </main>
   );
 }
